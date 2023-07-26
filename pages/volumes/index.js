92084cd913dcafa086094413773307c4a10cef80
Fabian Ruettgers <fabulon78@gmail.com>
2023-07-26 16:36:11 +0200

    finished

--- pages/volumes/index.js
+++ pages/volumes/index.js
@@ -1,26 +1,33 @@
 import Link from "next/link";
-import { introduction } from "../../lib/data";
+import { introduction, volumes } from "../../lib/data";
+import { useRouter } from "next/router";
 
 export default function Volumes() {
+  const router = useRouter();
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handleClick() {
+    const randomMovie = getRandomElement(volumes);
+    router.push(`/volumes/${randomMovie.slug}`);
+  }
+
   return (
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map((volume) => {
+          return (
+            <li key={volume.slug}>
+              <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+            </li>
+          );
+        })}
       </ul>
+      <button onClick={handleClick}>Random Movie</button>
     </>
   );
 }
